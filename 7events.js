d10e6a956c80d54e14cf9ad68d8ce2e9bd4a94e0
YasirHasn9 <hassanyasir223@gmail.com>
2020-07-20 15:04:32 -0700

    the differences between e.preventDefault() and e.stopPropagation()

---
+++ 7events.js
@@ -0,0 +1,10 @@
+// what is the differences between event.preventDefault()
+                            //  and   event.stopPropagation()
+
+/*
+event.preventDefault() is preventing the default behavior of an element 
+if it used in the form , it will prevent it form submitting
+
+event.stopPropagation() stops the propagation of element or stops from 
+the bubbling phase
+*/
