11eb6a66db8a615f20f79b82f3ad908775b37bfd
YasirHasn9 <hassanyasir223@gmail.com>
2020-07-20 14:54:42 -0700

    chunk of code about the bubble phase

--- 6eventPropagaion.js
+++ 6eventPropagaion.js
@@ -1,4 +1,4 @@
-                            // Event Propagation
+// Event Propagation
 /*
 When en event happened on the dom element , this event does not entirely occur
 of that one element and this leads us the phases of the events
@@ -28,3 +28,45 @@ window
                              ⬇️        ↑
                             button   bubble ↑
                      */
+
+// example about the bubble phase
+// imagine that we have this code in our html file
+/* 
+        <div class="grandparent">
+            <div class="parent">
+                 <div class="child">1</div>
+            </div>
+        </div>
+*/
+function addEvent(el, event , callback , isCapturing = false){
+    if(!el || !even || !callback || typeof callback !== function){
+        if(typeof el === "string"){
+            el = document.querySelector(el)
+        }
+        el.addEventListener(event , callback, isCapturing)
+    }
+}
+
+// call the function 
+addEvent(document , "DOMContentLoaded" , () => {
+    child = document.querySelector(".child")
+    parent = document.querySelector(".parent")
+    grandparent = document.querySelector(".grandparent")
+
+    addEvent(child, "click" , () => console.log("child"))
+    addEvent(parent, "click" , () => console.log("parent"))
+    addEvent(grandparent, "click" , () => console.log("grandparent"))
+    addEvent(document, 'click', function (e) {
+        console.log('document');
+      });
+    
+      addEvent('html', 'click', function (e) {
+        console.log('html');
+      })
+    
+      addEvent(window, 'click', function (e) {
+        console.log('window');
+      })    
+})
+
+// notice if click on the child element it will occur of every element until window 
