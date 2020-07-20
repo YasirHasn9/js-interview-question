6a05d9b556eed7bc2648be43336cd6e0810ddde5
YasirHasn9 <hassanyasir223@gmail.com>
2020-07-20 12:59:57 -0700

    what is dom

---
+++ DOM.js
@@ -0,0 +1,45 @@
+                // DOM
+/*
+Dom, stands for Document object model , it is interface for html and xml.
+when the browser reads of parses our html documents ,it creates a big
+object based on the document of the html in the dom.
+it is a tree-like structure that modeled form the document of the html. Dom is used 
+for interacting and modifying the dom's structure or specific element or nodes
+*/
+
+/*
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+   <title>Document Object Model</title>
+</head>
+
+<body>
+   <div>
+      <p>
+      </p>
+   </div>
+</body>
+
+</html>
+                    meta
+                    /
+document has -- head  -- meta
+            \     |    \ 
+             \    meta   \
+              \           title
+            body
+             |
+             div
+             |
+             p
+
+document is dom in js that allows us to choose element in the html and play with them
+*/
+
+
+
